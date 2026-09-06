0e2561ab83677c4f1ddddc1f12385f7eabac40d9
Amirali Mohsenifar <amirali213121@gmail.com>
2026-09-06 13:34:42 +0330

    Create response.codes.go

---
+++ response.codes.go
@@ -0,0 +1,17 @@
+package codes
+
+// A special type to prevent the use of invalid codes, 
+// for functions that require a code, name the type this way
+type Code string
+
+// Add your custom response codes
+const (
+	// success response codes
+	Ok       Code = "OK"
+	Healthly Code = "HEALTHLY"
+	// errors status codes
+	ServerErr      Code = "SERVER_ERROR"
+	ToManyRequests Code = "TO_MANY_REQUESTS"
+	NotFound       Code = "NOT_FOUND"
+	BadRequest     Code = "BAD_REQUEST"
+)
